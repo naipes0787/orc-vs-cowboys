f938458861485f3039216e191d6092796fa82bc1
Leandro Costa <leandrodavidcosta@gmail.com>
2017-08-28 13:24:26 -0300

    Initial commit of all the work until now

---
+++ Assets/Scripts/enemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour {
+	public float vel = -1f;
+	Rigidbody2D rgb;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rgb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	void FixedUpdate () {
+		Vector2 v = new Vector2 (vel, rgb.velocity.y);
+		rgb.velocity = v;
+		if ((anim.GetCurrentAnimatorStateInfo (0).IsName ("walking")) && 
+				(Random.value < (1f / (60f * 3f)))) {
+			anim.SetTrigger ("aim");
+		} else if (anim.GetCurrentAnimatorStateInfo (0).IsName ("aiming")){
+			if(Random.value < 1f / 3f) {
+				anim.SetTrigger ("shoot");
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Flip ();
+	}
+
+	void Flip(){
+		vel *= -1;
+		var s = transform.localScale;
+		s.x *= -1;
+		transform.localScale = s;
+	}
+}
---
+++ Assets/Scripts/mainCharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainCharacterController : MonoBehaviour {
+	Rigidbody2D rgb;
+	Animator anim;
+	public float maxVel = 5f;
+	bool toRight = true;
+
+	// Use this for initialization
+	void Start () {
+		rgb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void FixedUpdate () {
+		float v = Input.GetAxis ("Horizontal");
+		Vector2 vel = new Vector2 (0, rgb.velocity.y);
+		v *= maxVel;
+		vel.x = v;
+		rgb.velocity = vel;
+		anim.SetFloat ("speed", vel.x);
+		if (toRight && v < 0) {
+			toRight = false;
+			Flip ();
+		} else if (!toRight && v > 0) {
+			toRight = true;
+			Flip ();
+		}
+	}
+
+	void Flip(){
+		var s = transform.localScale;
+		s.x *= -1;
+		transform.localScale = s;
+	}
+}
